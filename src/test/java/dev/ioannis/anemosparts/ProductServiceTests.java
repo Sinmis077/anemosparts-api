f2c094949a408295fc42202c92829e2ffa0b89f7
Ioannis Panagi <llelga8@gmail.com>
2025-10-01 15:05:17 +0200

    Added extendswith mockitoextension class to fix the test failing from partRepo being null
    Signed-off-by: Ioannis Panagi <llelga8@gmail.com>

--- src/test/java/dev/ioannis/anemosparts/ProductServiceTests.java
+++ src/test/java/dev/ioannis/anemosparts/ProductServiceTests.java
@@ -3,13 +3,16 @@ package dev.ioannis.anemosparts;
 import dev.ioannis.anemosparts.business.impl.PartServiceImpl;
 import dev.ioannis.anemosparts.persistance.PartRepo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Set;
 
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTests {
     @Mock
     private PartRepo partRepo;
