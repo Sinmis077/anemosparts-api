b0767a1a23a75edc4fc6bd0415b8fe2758b9ef33
Ioannis Panagi <llelga8@gmail.com>
2025-12-01 03:31:34 +0100

    Made PartImage thumbnail field default to false.
    Signed-off-by: Ioannis Panagi <llelga8@gmail.com>

--- src/main/java/dev/ioannis/anemosparts/entities/PartImage.java
+++ src/main/java/dev/ioannis/anemosparts/entities/PartImage.java
@@ -22,7 +22,7 @@ public class PartImage {
     private String source;
 
     @ColumnDefault("false")
-    private Boolean thumbnail;
+    private Boolean thumbnail = false;
 
     @ManyToOne
     @JoinColumn(name = "part_id", referencedColumnName = "id")
